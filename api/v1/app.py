8f8561d7bf7249ecec6b79aa4661751c10219746
felixobianozie <felixobianozie@gmail.com>
2022-11-04 23:29:40 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,14 +4,14 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
-#from flask_cors import CORS
+from flask_cors import CORS
 #from flasgger import Swagger
 #from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-#app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-#cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -12,7 +12,7 @@ from flask import abort, jsonify, make_response, request
 @app_views.route('places/<place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
 #@swag_from('documentation/place_amenity/get_places_amenities.yml',
-           methods=['GET'])
+#           methods=['GET'])
 def get_place_amenities(place_id):
     """
     Retrieves the list of all Amenity objects of a Place
@@ -34,7 +34,7 @@ def get_place_amenities(place_id):
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
 #@swag_from('documentation/place_amenity/delete_place_amenities.yml',
-           methods=['DELETE'])
+#           methods=['DELETE'])
 def delete_place_amenity(place_id, amenity_id):
     """
     Deletes a Amenity object of a Place
@@ -65,7 +65,7 @@ def delete_place_amenity(place_id, amenity_id):
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
                  strict_slashes=False)
 #@swag_from('documentation/place_amenity/post_place_amenities.yml',
-           methods=['POST'])
+#           methods=['POST'])
 def post_place_amenity(place_id, amenity_id):
     """
     Link a Amenity object to a Place
